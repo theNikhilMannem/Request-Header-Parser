bb34497163c2ce448d0825c85412bd3303626f07
repl.it user <replituser@example.com>
2020-12-17 13:28:47 +0000

    commit after successfully passing the challenge tests

--- server.js
+++ server.js
@@ -24,7 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", (req, res) => {
+  console.log(req)
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.rawHeaders[3]
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
